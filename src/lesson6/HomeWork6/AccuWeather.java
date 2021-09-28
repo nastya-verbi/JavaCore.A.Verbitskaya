536f6163e69156283e2637ac07844d348babf186
nastya_verbi <blissena49@gmail.com>
2021-09-28 20:38:35 +0400

    ДЗ №6

---
+++ src/lesson6/HomeWork6/AccuWeather.java
@@ -0,0 +1,22 @@
+package lesson6.HomeWork6;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class AccuWeather {
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        Request request = new Request.Builder()
+        .url("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2711916c445d123957f963e549eb8e9b")
+        .build();
+
+        Response response = okHttpClient.newCall(request).execute();
+        System.out.println(response.body().string());
+
+    }
+}
